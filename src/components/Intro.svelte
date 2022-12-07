39786aa967d925300bf694448e8ce6160bf84bac
Tyson <TysonS.417@gmail.com>
2022-12-07 09:43:25 -0500

    create Intro component

---
+++ src/components/Intro.svelte
@@ -0,0 +1,27 @@
+<div id="intro">
+  <h1>Hello welcome to my NBA fantasy stats tool</h1>
+  <p>
+    This app was made possible with the balldontlie API created by <a
+      href="https://www.github.com/ynnadkrap">ynnadkrap</a
+    >
+  </p>
+  <p>
+    Fantasy scoring on this site are based on ESPN's old scoring style
+    calculated as follows:
+  </p>
+  <p>
+    Points + Rebounds + Steals + Blocks + (Field Goals Made - Field Goals
+    Attemped) + (Free Throws Made - Free Throws Attemped) + (Assists -
+    Turnovers)
+  </p>
+  <h3>Directions</h3>
+  <ol>
+    <li>Search and select up to 5 players</li>
+    <li>Browse through each players season averages</li>
+    <li>Select a date range</li>
+    <li>
+      Browse through each players stats through each game in chosen date range
+    </li>
+    <li>Compare players stats visually by using the graph</li>
+  </ol>
+</div>
