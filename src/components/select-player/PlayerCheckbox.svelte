84666b67a31f8125c8cc442a4a0621caee909e3d
Tyson <TysonS.417@gmail.com>
2022-12-07 09:43:03 -0500

    create PlayerCheckBox svelte component

---
+++ src/components/select-player/PlayerCheckbox.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let player = null;
+  let { id, position, first_name, last_name, team } = player;
+</script>
+
+<label>
+  <input type="checkbox" bind:value={id} id={id + "cbox"} />
+  {first_name}
+  {last_name} - {team.abbreviation}
+  {position}
+</label>
