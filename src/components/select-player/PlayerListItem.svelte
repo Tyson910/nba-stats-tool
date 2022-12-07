28894067dee5d18a505b4fb5f84d7517e577fad4
Tyson <TysonS.417@gmail.com>
2022-12-07 09:43:16 -0500

    create PlayerListItem svelte component

---
+++ src/components/select-player/PlayerListItem.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let player = null;
+  let { id, position, first_name, last_name, team } = player;
+</script>
+
+<li>
+  {first_name}
+  {last_name} - {team.abbreviation}
+  {position}
+  <span class=""> x </span>
+</li>
