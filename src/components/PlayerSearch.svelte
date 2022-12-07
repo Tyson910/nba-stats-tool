00dee6e771b3932391379a6acc9ffb164778cf33
Tyson <TysonS.417@gmail.com>
2022-12-07 09:44:11 -0500

    create PlayerSearch svelte component

---
+++ src/components/PlayerSearch.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export let userSearchInput = "";
+</script>
+
+<form class="flex flex-col" id="search-form">
+  <label class="flex flex-col" for="player-search"> Player Name </label>
+  <input
+    name="player-search"
+    id="player-search"
+    type="text"
+    required
+    bind:value={userSearchInput}
+  />
+  <input class="" type="submit" value="Search" />
+</form>
