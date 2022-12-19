856192d53c4f94234b66050f43e525a7d37158db
Tyson <TysonS.417@gmail.com>
2022-12-18 23:43:11 -0500

    add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import * as path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@local-types": path.resolve(__dirname, "./src/types"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
+  plugins: [svelte()],
+});
