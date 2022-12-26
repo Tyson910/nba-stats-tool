a24ad9b8ea5a472ffecc11ebc5aa3e0c51b4b889
Tyson <TysonS.417@gmail.com>
2022-12-26 09:09:40 -0600

    create PlayerWithSeasonAverage intreface

--- src/types/ball-dont-lie/index.d.ts
+++ src/types/ball-dont-lie/index.d.ts
@@ -2,9 +2,13 @@ import { Team } from "./Team";
 import { Player } from "./Player";
 import { PlayerSeasonAverage } from "./SeasonAverage";
 import { PlayerGameStat } from "./PlayerGameStat";
+interface PlayerWithSeasonAverage extends Player {
+  seasonStats: PlayerSeasonAverage;
+}
 export {
   Player,
   PlayerGameStat,
   PlayerSeasonAverage,
+  PlayerWithSeasonAverage,
   Team,
 };
