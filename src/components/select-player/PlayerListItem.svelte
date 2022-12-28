8fc10245968567ab27cd75c03b08d45333494f49
Tyson <TysonS.417@gmail.com>
2022-12-27 19:28:08 -0600

    add disabled and loading state for PlayerListItem Button

--- src/components/select-player/PlayerListItem.svelte
+++ src/components/select-player/PlayerListItem.svelte
@@ -29,18 +29,28 @@
 			</p>
 		</div>
 	</div>
-	<div class="flex-shrink-0">
+	<div class="flex-shrink-0 group">
 		<button
 			on:click
 			{disabled}
 			type="button"
-			class={`inline-flex items-center rounded-full border border-transparent 
-      bg-green-100 disabled:bg-gray-100 py-2 px-3 hover:bg-green-200 transition-colors 
-      duration-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}>
-			<span class="text-sm font-medium text-green-800">
-				Add <span class="sr-only">{fullName}</span>
-			</span>
-			<PlusIcon svgClasses="-mr-1 mr-0.5 h-5 w-5 text-green-500" />
+			class={`py-2 px-5 inline-flex items-center 
+			 rounded-full border border-current hover:bg-current
+			 disabled:bg-current
+			 group-hover:transition-colors group-hover:duration-300
+			 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2
+			 `}
+			style="color: {teamColorString};">
+			{#if disabled}
+				<span class="text-sm font-medium text-gray-50">
+					Already added <span class="sr-only">{fullName}</span>
+				</span>
+			{:else}
+				<span class="text-sm font-medium group-hover:text-gray-50">
+					Add <span class="sr-only">{fullName}</span>
+				</span>
+				<PlusIcon svgClasses="-mr-1 mr-0.5 h-5 w-5 group-hover:text-gray-50" />
+			{/if}
 		</button>
 	</div>
 </li>
