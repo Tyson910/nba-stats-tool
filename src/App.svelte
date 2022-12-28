0d170a070269f4da2162f4334771ce3998c07b3e
Tyson <TysonS.417@gmail.com>
2022-12-27 21:50:42 -0600

    create App component

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,36 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+	import { setContext } from "svelte";
+	import type { SelectedPlayer } from "@local-types/ball-dont-lie";
+	import Tag from "@components/Tag.svelte";
+	import Intro from "@components/Intro.svelte";
+	import PlayerSearch from "@components/PlayerSearch.svelte";
+	import Tabs from "@components/Tabs.svelte";
+
+	setContext("selectedPlayers", { addPlayerToSelectedPlayers, removePlayerFromSelectedPlayers });
+	function addPlayerToSelectedPlayers(player: SelectedPlayer): void {
+		// add to selected players
+		selectedPlayers = [...selectedPlayers, player];
+		// fetch players stats
+	}
+	function removePlayerFromSelectedPlayers(selectedPlayerID: SelectedPlayer["id"]): void {
+		// remove from search results
+		selectedPlayers = selectedPlayers.filter(({ id }) => id != selectedPlayerID);
+	}
+
+	let selectedPlayers: SelectedPlayer[] = [];
+	$: selectedPlayersIDs = selectedPlayers.map(({ id }) => id);
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+<main class="mx-auto max-w-screen-xl pb-20">
+	<Intro />
+	<div class="flex flex-row gap-x-4 my-5 capitalize">
+		{#each selectedPlayers as player (player.id)}
+			{@const fullName = player?.first_name + " " + player?.last_name}
+			<Tag on:click={() => removePlayerFromSelectedPlayers(player.id)}>{fullName}</Tag>
+		{/each}
+	</div>
+	<PlayerSearch {selectedPlayersIDs} />
+	{#if selectedPlayers.length > 0}
+		<Tabs {selectedPlayers} />
+	{/if}
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
