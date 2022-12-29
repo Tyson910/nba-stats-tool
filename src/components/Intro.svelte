1d4aafd0bd3634740aabe1c1e6d7399fdd4ca5b1
Tyson <TysonS.417@gmail.com>
2022-12-29 08:30:45 -0600

    update Intro

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -1,17 +1,25 @@
+<script>
+	import GithubIcon from "@components/icons/GithubIcon.svelte";
+</script>
+
 <div
 	id="intro"
 	class="prose max-w-screen-xl mx-auto">
 	<h1>Hello welcome to my NBA fantasy stats tool</h1>
-	<p>
-		This app was made possible with the balldontlie API created by <a
-			href="https://www.github.com/ynnadkrap"
-			target="blank"
-			rel="noopener noreferrer">ynnadkrap</a>
-	</p>
+	<div class="group">
+		<a
+			href="https://github.com/Tyson910/nba-stats-tool"
+			target="_blank"
+			rel="noreferrer nooopener"
+			class="flex flex-row items-center gap-x-2 no-underline  group-hover:underline"
+			>Source Code <GithubIcon svgClasses="group-hover:text-gray-500" /></a>
+	</div>
 	<p>Fantasy scoring on this site are based on ESPN's old scoring style calculated as follows:</p>
 	<p>
-		Points + Rebounds + Steals + Blocks + (Field Goals Made - Field Goals Attemped) + (Free Throws
-		Made - Free Throws Attemped) + (Assists - Turnovers)
+		<strong>Points</strong> + <strong>Rebounds</strong> + <strong>Steals</strong> +
+		<strong>Blocks</strong> + (<strong>Field Goals Made</strong> -
+		<strong>Field Goals Attemped</strong>) + (<strong>Free Throws Made</strong> -
+		<strong>Free Throws Attemped</strong>) + (<strong>Assists</strong> - <strong>Turnovers</strong>)
 	</p>
 	<h3>Directions</h3>
 	<ol>
