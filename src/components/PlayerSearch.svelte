941944e2fd434ee8df99abd483367cc497c5bf04
Tyson <TysonS.417@gmail.com>
2023-08-26 08:29:47 -0400

    design: new search bar ui

--- src/components/PlayerSearch.svelte
+++ src/components/PlayerSearch.svelte
@@ -83,32 +83,38 @@
 	class="flex flex-col gap-y-2 max-w-lgmx-auto"
 	id="search-form"
 	on:submit|preventDefault={handlePlayerSearchSubmit}>
-	<label
-		class="flex flex-col font-medium text-gray-700"
-		for="player-search">
-		Player Name
-	</label>
-	<input
-		name="player-search"
-		id="player-search"
-		type="text"
-		class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
-		required
-		bind:value={userSearchInput} />
-	<p
-		class="text-sm text-red-500"
-		id="email-description">
-		{playerSearchErrorMessage}
-	</p>
-	<button
-		class="bg-indigo-600 text-green-50 w-max self-end py-2 px-3 rounded-lg inline-flex items-center"
-		type="submit"
-		class:animate-pulse={isLoading}
-		>Search
-		{#if isLoading}
-			<Spinner />
-		{/if}
-	</button>
+	<div>
+		<label
+			class="flex flex-col font-medium text-gray-700"
+			for="player-search">
+			Player Name
+		</label>
+		<p
+			class="text-sm text-red-500"
+			id="player-search-error-message">
+			{playerSearchErrorMessage}
+		</p>
+		<div class="mt-2 flex rounded-md shadow-sm">
+			<input
+				type="search"
+				name="player-search"
+				id="player-search"
+				class="w-full min-w-0 flex-1 rounded-none rounded-l-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+				placeholder="E.g. Keegan Murray"
+				required
+				bind:value={userSearchInput} />
+
+			<button
+				class="bg-indigo-600 py-2 px-5 text-green-50 inline-flex items-center rounded-r-md border border-l-0 border-gray-300 sm:text-sm"
+				type="submit"
+				class:animate-pulse={isLoading}
+				>Search
+				{#if isLoading}
+					<Spinner />
+				{/if}
+			</button>
+		</div>
+	</div>
 </form>
 <section class="my-4">
 	{#each playerSearchResults as player (player.id)}
